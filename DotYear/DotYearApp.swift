8904dc6a678421cbae3f5354eba52592338de53b
kemo <kemo.jallow@hannlync.com>
2025-01-18 02:40:46 +0800

    Initial Commit

---
+++ DotYear/DotYearApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotYearApp.swift
+//  DotYear
+//
+//  Created by Kemo Jallow on 2025/1/18.
+//
+
+import SwiftUI
+
+@main
+struct DotYearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DotYearTests/DotYearTests.swift
@@ -0,0 +1,17 @@
+//
+//  DotYearTests.swift
+//  DotYearTests
+//
+//  Created by Kemo Jallow on 2025/1/18.
+//
+
+import Testing
+@testable import DotYear
+
+struct DotYearTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
